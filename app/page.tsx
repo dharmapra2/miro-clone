1e0a4d4aa375249046a8ae806a9a305665dacd98
Dharmapra2 <dharmapra865@gmail.com>
2024-09-20 22:56:43 +0530

    Added shadcn/ui

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      heea f;asdfjkas jdkf sldf alks;d fjkalsd jf;asdfasdflo
+      <Button>Click me</Button>
+    </main>
+  );
+}
