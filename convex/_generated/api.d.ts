63abe6b91d177887be89079cf38e378ada7d35b8
Dharmapra2 <dharmapra865@gmail.com>
2025-03-23 16:53:44 +0530

    update convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -1,5 +1,3 @@
-/* prettier-ignore-start */
-
 /* eslint-disable */
 /**
  * Generated `api` utility.
@@ -33,5 +31,3 @@ export declare const internal: FilterApi<
   typeof fullApi,
   FunctionReference<any, "internal">
 >;
-
-/* prettier-ignore-end */
